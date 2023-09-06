e879585d7afa0ecd8934cc5456c774be053518de
Shivam Kumar Jha <shivamjhaaaa@gmail.com>
2023-09-06 22:15:40 +0530

    quickSort

--- DSA/src/Recursion/Lec5/InPlace.java
+++ DSA/src/Recursion/Lec5/InPlace.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class InPlace {
     public static void main(String[] args) {
-        int[] arr = {1,8,9,5,11,32,5,6,7,1};
+        int[] arr = {5,4,3,2,1};
         mergeSort(arr,0,arr.length);
         System.out.println(Arrays.toString(arr));
 
---
+++ DSA/src/Recursion/Lec6/QuickSort.java
@@ -0,0 +1,47 @@
+
+package Recursion.Lec6;
+
+import java.util.Arrays;
+
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quickSort(int[] arr,int low, int high){
+        if(low>=high){
+            return;
+        }
+
+        int start = low;
+        int end = high;
+        int mid = start + (end - start)/2;
+
+        // put the pivot to the correct index
+        while (start<=end) {
+            while(arr[start] < arr[mid]){
+                start++;
+            }
+            while(arr[end] > arr[mid]){
+                end--;
+            }
+
+            // swap the elements
+            if(start <= end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        
+        // sort the left and the right array
+    
+        quickSort(arr,low,end);
+        quickSort(arr,start,high);
+    }
+}
